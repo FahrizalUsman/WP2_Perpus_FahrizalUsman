4e87ce2d02a396e040e8e0a1b9383c591edf7c9c
FahrizalUsman <fahrizalu260601@gmail.com>
2021-10-19 21:57:05 +0700

    Upload Tugas Pert.6

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+ <title>Latihan 1</title>
+</head>
+<body>
+ <br><b>Halo Kawan</b>.. Yuk kita belajar web programming..!!!</br>
+ <i>Nilai 1</i> = <?= $nilai1; ?>
+ <br><i>Nilai 2</i> = <?= $nilai2; ?></br>
+ ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
